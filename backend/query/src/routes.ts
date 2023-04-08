cfb77ea51dab4fdaeeb2e05fc20d48e2a055c3a0
gggfox <gerardogalangarzafox@gmail.com>
2023-04-08 14:12:34 -0600

    feat: add post query service

---
+++ backend/query/src/routes.ts
@@ -0,0 +1,50 @@
+import express, { application } from "express";
+const router = express.Router();
+import { Request, Response } from "express";
+import { body, validationResult } from 'express-validator';
+import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
+
+interface Comment {
+  id: string;
+  content: string
+}
+
+interface Post {
+  id: string;
+  title: string
+  comments: Comment[]
+}
+
+const posts = new Map<string, Post>();
+
+router.get("/posts/:id", function (req: Request, res: Response) {
+  const post = posts
+  res.status(200).send(post)
+});
+
+router.get("/posts", function (req: Request, res: Response) {
+});
+
+router.post('/events', function(req: Request, res: Response) {
+  const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const {id, title} = data;
+    const post: Post = { id, title, comments: [] }
+    posts.set(id, post)
+  }
+  if (type === "CommentCreated") {
+    const {id, content , postId} = data;
+    if (!posts.has(postId)) {
+      throw new Error("trying to add comment to non existing post")
+    }
+    const post:Post = posts.get(postId)!;
+    const comment: Comment = {id, content}
+    post?.comments.push(comment)
+    posts.set(postId, post)
+  }
+  console.log(posts)
+  res.send({})
+});
+
+export default router;
